7aa4e4f8892f10fbb8ec80d85ec5730c1dec1c72
chunsinglau <chunsinglau@10.0.0.107>
2017-03-14 17:47:35 +0800

    insert the custom account

--- spring-boot-oauth-demo/src/main/java/com/singplayground/Oauth2AuthorizationServerApplication.java
+++ spring-boot-oauth-demo/src/main/java/com/singplayground/Oauth2AuthorizationServerApplication.java
@@ -6,8 +6,19 @@ import java.security.Principal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -16,6 +27,8 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.singplayground.model.Account;
  
 @SpringBootApplication
 @RestController
@@ -25,6 +38,59 @@ public class Oauth2AuthorizationServerApplication extends WebMvcConfigurerAdapte
 	public static void main(String[] args) {
 		SpringApplication.run(Oauth2AuthorizationServerApplication.class, args);
 	}
+	
+	
+
+@Configuration
+class WebSecurityConfiguration extends GlobalAuthenticationConfigurerAdapter {
+
+  //@Autowired
+  //AccountRepository accountRepository;
+
+  @Override
+  public void init(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(userDetailsService());
+  }
+
+  @Bean
+  UserDetailsService userDetailsService() {
+    return new UserDetailsService() {
+
+      @Override
+      public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+       // Account account = accountRepository.findByUsername(username);
+       System.out.println("XXXXXXXXXX ");
+       System.out.println("wwwwww " + username);
+       Account account = new Account(); 
+       account.setId("12345");
+       account.setPassword("agentsecretx");
+       account.setUsername("agentx");
+      if(account != null) {
+        return new User(account.getUsername(), account.getPassword(), true, true, true, true,
+                AuthorityUtils.createAuthorityList("USER"));
+        } else {
+          throw new UsernameNotFoundException("could not find the user '"
+                  + username + "'");
+        }
+      }
+      
+    };
+  }
+}
+
+@EnableWebSecurity
+@Configuration
+class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+ 
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests().anyRequest().fullyAuthenticated().and().
+    httpBasic().and().
+    csrf().disable();
+  }
+}
+	
+	
  
 	@Configuration
 	@EnableAuthorizationServer
@@ -35,17 +101,20 @@ public class Oauth2AuthorizationServerApplication extends WebMvcConfigurerAdapte
 		@Override
 		public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 			endpoints.authenticationManager(authenticationManager);
+		
 		}
  
 		@Override
 		public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-			clients.inMemory().withClient("foo").secret("foosecret")
+			clients.inMemory().withClient("iris").secret("irissecret")
 					.authorizedGrantTypes("authorization_code", "refresh_token", "password").scopes("openid");
 		}
 	}
  
 	@RequestMapping("/user")
 	public Principal user(Principal user) {
+		System.out.println("--------" );
+		System.out.println(user.getName());
 		return user;
 	}
  
